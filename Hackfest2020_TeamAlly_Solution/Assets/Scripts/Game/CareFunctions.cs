ef25fb6d5c50cb2a6023bdb047f39e4d2f75b2bb
JJPadilla <jjpadilla0915@gmail.com>
2020-11-28 04:04:21 +0800

    added fertilizer and a little work on adjusting light opacity

--- Hackfest2020_TeamAlly_Solution/Assets/Scripts/Game/CareFunctions.cs
+++ Hackfest2020_TeamAlly_Solution/Assets/Scripts/Game/CareFunctions.cs
@@ -6,21 +6,20 @@ public class CareFunctions : MonoBehaviour
 {
     [Header("Care Item Parents")]
     public GameObject wateringPailParent;
+    public GameObject fertilizePlantParent;
 
     [Header("Care Items")]
     public GameObject sunSlider;
+
+    [Header("Other")]
+    public GameObject spotLight;
 
     // Start is called before the first frame update
     void Start()
     {
         wateringPailParent.SetActive(false);
         sunSlider.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        fertilizePlantParent.SetActive(false);
     }
 
     public void waterPlant()
@@ -33,8 +32,19 @@ public class CareFunctions : MonoBehaviour
         sunSlider.SetActive(true);
     }
 
+    public void adjustLight(float opacity)
+    {
+        spotLight.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, -opacity);
+        Debug.Log(opacity);
+    }
+
     public void sunSliderHide()
     {
         sunSlider.SetActive(false);
+    }
+
+    public void fertilizePlant()
+    {
+        fertilizePlantParent.SetActive(true);
     }
 }
