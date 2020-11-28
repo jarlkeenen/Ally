92b9e8c6698932bd0204da530dd8aa007bc5b080
JJPadilla <jjpadilla0915@gmail.com>
2020-11-28 17:42:45 +0800

    adjusted top canvas confirmation panel and prepared health bar to be more flexible

---
+++ Hackfest2020_TeamAlly_Solution/Assets/Scripts/Game/Healthbar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    public Slider slider; 
+
+    //adjust health
+    public void setHealth(int health)
+    {
+        slider.value = health;
+    }
+
+    //set max health through script
+    public void setMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
