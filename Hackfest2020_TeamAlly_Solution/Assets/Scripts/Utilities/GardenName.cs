341a99c5ad283a51ef6f680abcb7ac8730b4269e
JJPadilla <jjpadilla0915@gmail.com>
2020-11-29 04:53:12 +0800

    Linked slider in settings to volume and adjusted names in top canvas

---
+++ Hackfest2020_TeamAlly_Solution/Assets/Scripts/Utilities/GardenName.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GardenName : MonoBehaviour
+{
+    public TextMeshProUGUI gardenName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gardenName.text = PlantDataHandler.instance.gardenName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
